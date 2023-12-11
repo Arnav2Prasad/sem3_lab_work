28a15a4fbbb6b318cda1aa657b1982ca73ebc1e6
Arnav Prasad <122086367+Arnav2Prasad@users.noreply.github.com>
2023-12-11 23:05:43 +0530

    Update q2.c

--- lab_work_structure/q2.c
+++ lab_work_structure/q2.c
@@ -1,69 +1,85 @@
 #include <stdio.h>
-#include <string.h>
 
-#define MAX_HOTELS 5
-#define MAX_NAME_LENGTH 50
-#define MAX_ADDRESS_LENGTH 100
+// Typedefs for readability
+typedef struct {
+    int day;
+    int month;
+    int year;
+} DateOfBirth;
 
 typedef struct {
-    char name[MAX_NAME_LENGTH];
-    char address[MAX_ADDRESS_LENGTH];
-    int grade;
-    int numberOfRooms;
-    float roomCharges;
-} Hotel;
+    float english;
+    float mathematics;
+    float computerScience;
+} Marks;
 
-void printHotelsByGrade(Hotel hotels[], int count, int grade) {
-    printf("\nHotels in Grade %d:\n", grade);
-    for (int i = 0; i < count; i++) {
-        if (hotels[i].grade == grade) {
-            printf("%s\n", hotels[i].name);
-        }
-    }
+typedef struct {
+    char firstName[50];
+    char middleName[50];
+    char lastName[50];
+} Name;
+
+typedef struct {
+    Name name;
+    char rollNumber[20];
+    char gender;
+    DateOfBirth dob;
+    Marks marks;
+} Student;
+
+// Function to calculate the aggregate percentage
+float calculateAggregate(const Marks* marks) {
+    return (marks->english + marks->mathematics + marks->computerScience) / 3.0;
 }
 
-void printHotelsWithRoomChargesLessThan(Hotel hotels[], int count, float charge) {
-    printf("\nHotels with Room Charges less than %.2f:\n", charge);
-    for (int i = 0; i < count; i++) {
-        if (hotels[i].roomCharges < charge) {
-            printf("%s\n", hotels[i].name);
-        }
-    }
+// Function to display student details
+void displayStudentDetails(const Student* student) {
+    printf("\nStudent Details\n");
+    printf("Name: %s %s %s\n", student->name.firstName, student->name.middleName, student->name.lastName);
+    printf("Roll Number: %s\n", student->rollNumber);
+    printf("Gender: %c\n", student->gender);
+    printf("Date of Birth: %d/%d/%d\n", student->dob.day, student->dob.month, student->dob.year);
+    printf("Marks - English: %.2f, Mathematics: %.2f, Computer Science: %.2f\n",
+           student->marks.english, student->marks.mathematics, student->marks.computerScience);
 }
 
 int main() {
-    Hotel hotels[MAX_HOTELS] = {
-        {"Hotel A", "Address A", 3, 50, 75.0},
-        {"Hotel B", "Address B", 4, 60, 90.0},
-        {"Hotel C", "Address C", 3, 40, 60.0},
-        {"Hotel D", "Address D", 2, 30, 50.0},
-        {"Hotel E", "Address E", 4, 45, 85.0}
+    // Sample data for three students
+    Student students[3] = {
+        {{"John", "", "Doe"}, "S001", 'M', {1, 5, 2000}, {80, 75, 90}},
+        {{"Jane", "Ann", "Smith"}, "S002", 'F', {15, 8, 2001}, {60, 45, 50}},
+        {{"Alice", "Eve", "Johnson"}, "S003", 'F', {10, 12, 1999}, {30, 40, 35}},
     };
 
-    int choice;
-    printf("Select an operation:\n");
-    printf("1. Print hotels in a particular grade\n");
-    printf("2. Print hotels with room charges less than specified value\n");
-    scanf("%d", &choice);
+    // Get user input for the name to search
+    char searchName[50];
+    printf("Enter the name of the student to search: ");
+    scanf("%s", searchName);
 
-    switch (choice) {
-        case 1:
-            printf("\nEnter the grade: ");
-            int grade;
-            scanf("%d", &grade);
-            printHotelsByGrade(hotels, MAX_HOTELS, grade);
+    // Display details of the student with the entered name
+    int found = 0;
+    for (int i = 0; i < 3; ++i) {
+        if (strcmp(students[i].name.firstName, searchName) == 0 ||
+            strcmp(students[i].name.middleName, searchName) == 0 ||
+            strcmp(students[i].name.lastName, searchName) == 0) {
+            displayStudentDetails(&students[i]);
+            found = 1;
             break;
+        }
+    }
 
-        case 2:
-            printf("\nEnter the maximum room charges: ");
-            float maxCharge;
-            scanf("%f", &maxCharge);
-            printHotelsWithRoomChargesLessThan(hotels, MAX_HOTELS, maxCharge);
-            break;
+    if (!found) {
+        printf("Student with name '%s' not found.\n", searchName);
+    }
 
-        default:
-            printf("Invalid choice!\n");
-            break;
+    // Display students with less than 40% aggregate
+    printf("\nStudents with less than 40%% aggregate:\n");
+    for (int i = 0; i < 3; ++i) {
+        float aggregate = calculateAggregate(&students[i].marks);
+        if (aggregate < 40.0) {
+            printf("%s %s %s - Aggregate: %.2f%%\n",
+                   students[i].name.firstName, students[i].name.middleName, students[i].name.lastName, aggregate);
+        }
     }
 
     return 0;
