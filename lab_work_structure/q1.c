5b621a5bd43516e034b41e3665c991c2a2936a5f
Arnav Prasad <122086367+Arnav2Prasad@users.noreply.github.com>
2023-12-11 22:59:16 +0530

    Update q1.c

--- lab_work_structure/q1.c
+++ lab_work_structure/q1.c
@@ -1,84 +1,49 @@
 #include <stdio.h>
-#include <string.h>
 
 typedef struct {
     int day;
     int month;
     int year;
-} Date;
+} DateOfBirth;
 
 typedef struct {
-    int english;
-    int mathematics;
-    int computerScience;
+    float english;
+    float mathematics;
+    float computerScience;
 } Marks;
 
 typedef struct {
-    int rollNumber;
     char firstName[50];
     char middleName[50];
     char lastName[50];
+} Name;
+
+typedef struct {
+    int roll
+    int rollNumber;
+    Name name;
     char gender;
-    Date dateOfBirth;
+    DateOfBirth dob;
     Marks marks;
 } Student;
 
-float calculateAggregate(Marks marks) {
-    return (marks.english + marks.mathematics + marks.computerScience) / 3.0;
-}
-
-void displayStudentDetails(Student student) {
-    printf("Roll Number: %d\n", student.rollNumber);
-    printf("Name: %s %s %s\n", student.firstName, student.middleName, student.lastName);
-    printf("Gender: %c\n", student.gender);
-    printf("Date of Birth: %d-%d-%d\n", student.dateOfBirth.day, student.dateOfBirth.month, student.dateOfBirth.year);
-    printf("Marks - English: %d, Mathematics: %d, Computer Science: %d\n", 
-           student.marks.english, student.marks.mathematics, student.marks.computerScience);
-}
-
 int main() {
-    Student students[5];
+    // Example usage:
+    Student student1;
 
-    for (int i = 0; i < 5; i++) {
-        printf("\nEnter details for Student %d\n", i + 1);
-        printf("Roll Number: ");
-        scanf("%d", &students[i].rollNumber);
-        printf("First Name: ");
-        scanf("%s", students[i].firstName);
-        printf("Middle Name: ");
-        scanf("%s", students[i].middleName);
-        printf("Last Name: ");
-        scanf("%s", students[i].lastName);
-        printf("Gender: ");
-        scanf(" %c", &students[i].gender);
-        printf("Date of Birth (DD MM YYYY): ");
-        scanf("%d %d %d", &students[i].dateOfBirth.day, &students[i].dateOfBirth.month, &students[i].dateOfBirth.year);
-        printf("Marks in English, Mathematics, Computer Science: ");
-        scanf("%d %d %d", &students[i].marks.english, &students[i].marks.mathematics, &students[i].marks.computerScience);
-    }
+    // Assign values to the structure members
+    student1.rollNumber = 12345;
+    student1.name = {"John", "Doe", ""}; 
+    student1.gender = 'M';
+    student1.dob = {1, 1, 2000};
+    student1.marks = {85.5, 90.0, 78.5};
 
-    char searchName[50];
-    printf("\nEnter the name of the student to search: ");
-    scanf("%s", searchName);
-
-    for (int i = 0; i < 5; i++) {
-        if (strcmp(searchName, students[i].firstName) == 0 ||
-            strcmp(searchName, students[i].middleName) == 0 ||
-            strcmp(searchName, students[i].lastName) == 0) {
-            printf("\nStudent details:\n");
-            displayStudentDetails(students[i]);
-            break;
-        }
-    }
-
-    printf("\nStudents who scored less than 40%% of the aggregate:\n");
-    for (int i = 0; i < 5; i++) {
-        float aggregate = calculateAggregate(students[i].marks);
-        if (aggregate < 40.0) {
-            printf("%s %s %s (Average Marks: %.2f)\n", 
-                   students[i].firstName, students[i].middleName, students[i].lastName, aggregate);
-        }
-    }
+    // Access and print the information
+    printf("Roll Number: %d\n", student1.rollNumber);
+    printf("Name: %s %s %s\n", student1.name.firstName, student1.name.middleName, student1.name.lastName);
+    printf("Gender: %c\n", student1.gender);
+    printf("Date of Birth: %d/%d/%d\n", student1.dob.day, student1.dob.month, student1.dob.year);
+    printf("Marks: English=%.2f, Mathematics=%.2f, Computer Science=%.2f\n", student1.marks.english, student1.marks.mathematics, student1.marks.computerScience);
 
     return 0;
 }
